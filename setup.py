d8635911bfd3b3a2cc184be216dcd1776dc8f242
crackofdawn <tentimes2@yahoo.com>
2018-05-31 10:53:17 -0400

    Added setup.py and initial module file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pymuddy',
+      version='0.1',
+      description='Python 3 MUD Framework',
+      url='http://github.com/crackofdawn/pymuddy',
+      author='crackofdawn',
+      author_email='tentimes2@yahoo.com',
+      license='None',
+      packages=['pymuddy'],
+      zip_safe=False)
